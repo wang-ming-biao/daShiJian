c122140553ac442839c1b4d0385c500d7e28efa8
wang-ming-biao <1340921277>
2019-12-06 16:56:50 +0800

    完成分页插件功能

--- BigNews/admin/js/article_category.js
+++ BigNews/admin/js/article_category.js
@@ -3,10 +3,11 @@
 $(function(){
     /*页面加载之后发送ajax请求所有文章分类 */
     $.ajax({
+        // 通过传参值调取地址
         url: BigNew.category_list,
         type:'get',
         dataType:'json',
-        data:'',
+        // data:'',
         success: function(backData){
         console.log(backData);
         // 数据响应之后 模板引擎渲染页面
@@ -42,6 +43,8 @@ $(function(){
     */
    
    /*2.新增文章类别 */
+// show 方法调用之后立即触发该事件。如果是通过点击某个作为触发器的元素，
+// 则此元素可以通过事件的 relatedTarget 属性进行访问。
     $('#myModal').on('shown.bs.modal',function(e) {
         if (e.relatedTarget == $('#xinzengfenlei')[0]) {
             // 新增按钮
@@ -50,10 +53,13 @@ $(function(){
             // 编辑按钮
             $('.btn-confirm').text('编辑');
             // 传递文章name
+            // 找到当前点击选项的父元素的上一个兄弟的上一个兄弟的文本
             $('#recipient-name').val($(e.relatedTarget).parent().prev().prev().text());
             // 传递文章的slug
+            // 找到当前点击选项的父元素的上一个兄弟的文本
             $('#message-text').val($(e.relatedTarget).parent().prev().text());
             // 传递文章的id
+            // 通过dom元素的attr拿到当前点击的编辑按钮的data-id,赋值给新增按钮
             $('.confirm').attr('data-id',$(e.relatedTarget).attr('data-id'));
         }
     });
@@ -62,7 +68,7 @@ $(function(){
     // 模态框新增按钮
     $('.btn-confirm').click(function(){
         if ($(this).text() == '新增') {
-            console.log('新增');
+            console.log('点击新增');
             $.ajax({
                 url:BigNew.category_add,
                 type:'post',
@@ -84,15 +90,20 @@ $(function(){
             /*3.编辑文章类别 */
             $.ajax({
                 url:BigNew.category_edit,
-                type:'get',
+                type:'post',
                 dataType:'json',
                 data:{
                     name:$('#recipient-name').val(),
                     slug:$('#message-text').val(),
+                    // 通过编辑按钮赋值一个data-id给新增按钮,再通过this获取到data-id
                     id:$(this).attr('data-id')
                 },
                 success: function(backData){
-            
+                if (backData.code == 200) {
+                    alert('编辑成功');
+                    // 刷新页面
+                    window.location.reload();
+                }
                 }
             });
         }
@@ -102,11 +113,35 @@ $(function(){
     
     $('.btn-cancel').click(function(){
         // 表单清空
+        // 重置表单 : DOM对象的reset()方法
         $('.modal-body>form')[0].reset();
     })
 
 
 
 
-   /*4.删除文章类别 */
+   /*4.删除文章类别 
+   注意点: 删除按钮是模板引擎动态添加的,需要注册委托事件
+   */
+//   将tbody原有的注册事件委托给'btn-delete'
+    $('.table>tbody').on('click','.btn-delete',function() {
+        // 获取当前点击的按钮的id;
+        var id = $(this).attr('data-id');
+        $.ajax({
+            url:BigNew.category_delete,
+            type:'post',
+            dataType:'json',
+            // 将上面获取的id作为属性值传给服务器
+            // 左边属性名,右边属性值
+            data:{ id:id },
+            success: function(backData){
+                // 若返回值等于204则代表成功
+            if (backData.code == 204) {
+                alert('删除成功');
+                // 刷新页面
+                window.location.reload();
+            }
+            }
+        });
+    })
 });
---
+++ BigNews/admin/js/article_list.js
@@ -0,0 +1,180 @@
+$(function () {
+    // 入口函数
+    /*解题思路
+    1. 页面一加载: 默认请求数据
+    
+    2.筛选按钮点击事件
+    筛选按钮  #btnSearch
+    文章类别下拉菜单 : #selCategory
+    文章状态下拉菜单 : #selStatus
+    */
+    // 页面加载后默认请求数据
+    // $('#selCategory')
+    // $.ajax({
+    //     url: BigNew.article_query,
+    //     type: 'get',
+    //     dataType: 'json',
+    //     data: {
+    //         page: 1,
+    //         perPage: 10
+    //     },
+    //     success: function (backData) {
+    //         console.log(backData);
+    //         // 将内容渲染给表单时,将其赋值给tbody
+    //         $('.table>tbody').html(template('art-list', backData));
+    //         // 01.默认加载之后初始化分页插件
+    //         // (1)先销毁已经存在的插件
+    //         $('#pagination').twbsPagination('destroy');
+    //         // (2)初始化新的插件
+    //         $('#pagination').twbsPagination({
+    //             totalPages: backData.data.totalPage,//总页数
+    //             startPage: 1,//起始页数
+    //             visiblePages: 6,//可见页数
+    //             first: "首页",
+    //             prev: "上一页",
+    //             next: "下一页",
+    //             last: "尾页",
+    //             onPageClick: function (event, page) {
+    //                 $('#page-content').text('Page ' + page);
+    //             }
+    //         })
+    //     }
+    // });
+    /*02.筛选按钮点击事件 
+    筛选按钮 : #btnSearch
+    注意点: 表单按钮要阻止默认行为
+    文章类别下拉菜单 : #selCategory
+    文章状态下拉菜单 : #selStatus
+    */
+
+    $('#btnSearch').click(function (e) {
+        // 02.1 阻止表单默认跳转行为
+        e.preventDefault();
+        // 02.2 ajax发送请求
+        getArticleList(1,true);
+        // $.ajax({
+        //     url: BigNew.article_query,
+        //     type: 'get',
+        //     dataType: 'json',
+        //     data: {
+        //         type: $('#selCategory').val(),
+        //         state: $('#selStatus').val(),
+        //         page: 1,
+        //         perpage: 10
+        //     },
+        //     success: function (backData) {
+        //         console.log(backData);
+        //         // 将内容渲染给表单时,将其赋值给tbody
+        //         $('.table>tbody').html(template('art-list', backData));
+        //         // 01.默认加载之后初始化分页插件
+        //         // 调用下面封装好的函数,传入系统返回的总页数,以及起始页数
+        //         loadPagination(backData.data.totalPage,1);
+        //     }
+        // });
+    })
+    // 主动触发文件加载程序
+    $('#btnSearch').trigger('click');
+    
+    /**
+    * @description: 加载分页插件
+    * @param {type} 
+    * @return: totalPage : 总页数
+    * @return: startPage : 起始页数
+    */
+    function loadPagination(totalPage,startPage){
+        // (1)先销毁已经存在的插件
+        $('#pagination').twbsPagination('destroy');
+        // (2)初始化新的插件
+        $('#pagination').twbsPagination({
+            totalPages: totalPage,//总页数
+            startPage: startPage,//起始页数
+            visiblePages: 6,//可见页数
+            first: "首页",
+            prev: "上一页",
+            next: "下一页",
+            last: "尾页",
+            onPageClick: function (event, page) {
+                // $('#page-content').text('Page ' + page);
+                // 在这里调用分页插件的封装函数,使得内容跟随页码变化
+                if (startPage != page) {
+                    getArticleList(page)
+                }
+            }
+        })
+    }
+
+
+    /*03. 分页插件 : 点击页码展示对应页码数据*/
+    /**
+    * @description: 根据页码请求文章列表数据
+    * @param {type} currentPage : 需要请求的页数
+    * @return: 
+    */
+    function getArticleList(currentPage,flag) {
+        $.ajax({
+            url: BigNew.article_query,
+            type: 'get',
+            dataType: 'json',
+            data: {
+                type: $('#selCategory').val(),
+                state: $('#selStatus').val(),
+                page: currentPage,
+                perpage: 10
+            },
+            success: function(backData) {
+                // 将内容渲染给表单时,将其赋值给tbody
+                $('.table>tbody').html(template('art-list', backData));
+                // 如果是筛选按钮,需要加载分页插件, 如果点击的是页面,则不需要重新加载分页插件
+                // 加载分页插件
+                if (flag) {
+                    loadPagination(backData.data.totalPage,currentPage);
+                }
+            }
+        });
+    }
+
+
+
+    /* 04.删除 : */
+    $('.delete').click(function () {
+            // $.ajax({
+            //     url: BigNew.article_delete,
+            //     type:'post',
+            //     dataType:'json',
+            //     data:{
+            //         id:
+            //     },
+            //     success: function(backData){
+            //     alert(backData.msg);
+            //     }
+            // });
+            console.log(totalCount);
+        })
+
+
+
+    /* 05.编辑 : a标签href跳转 (页面间传参)*/
+    /* 06. 发布 : a标签href跳转 */
+
+
+
+
+
+
+
+
+    // 页面加载之后发送ajax请求所有分类,并加载到分类框中
+
+    $.ajax({
+            url: BigNew.category_list,
+            type: 'get',
+            dataType: 'json',
+            // data:'',
+            success: function (backData) {
+                // 模板引擎渲染时是渲染给父元素,
+                $('#selCategory').html(template('cat-list', backData));
+                console.log(backData);
+            }
+        });
+
+    })
